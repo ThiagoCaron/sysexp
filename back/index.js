d8a130e3116e02d31ee20c9d890924bed2a646c2
Thiago Caron <thiagocaron@outlook.com>
2023-03-17 19:06:56 -0300

    aula final quinta

---
+++ back/index.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const app = express()
+const port = 3000;
+
+var csv =  require("node-csv").createParser();
+
+
+// CRUD
+
+// Creat -> Criar
+// Read -> Ler
+// Update -> Atualizar
+// Delete -> Apagar
+
+app.post("/entradas", function(req, res)
+{
+
+});
+
+app.get("/entradas", function(req, res)
+{
+    csv.parseFile("estoque.csv", function(erro, valores)
+    {
+        res.json(valores);
+    });
+});
+
+app.listen(port, () => 
+{
+    console.log("Example app listening on port ${port}")
+});
