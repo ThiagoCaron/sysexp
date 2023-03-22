cdb067c131bc4273540a66bc93992ceca0da9fd0
Thiago Caron <thiagocaron@outlook.com>
2023-03-21 21:54:39 -0300

    final da aula dia 21-03

--- back/index.js
+++ back/index.js
@@ -1,8 +1,15 @@
+const { json } = require("express");
 const express = require("express")
 const app = express()
 const port = 3000;
 
 var csv =  require("node-csv").createParser();
+
+const mongodb = require("mongodb");
+
+const url_mongo = "mongodb+srv://ThiagoCaronServi:ThiagoCaron@cluster0.6sctrse.mongodb.net/?retryWrites=true&w=majority"
+
+const conexao = new mongodb.MongoClient(url_mongo);
 
 
 // CRUD
@@ -23,6 +30,17 @@ app.get("/entradas", function(req, res)
     {
         res.json(valores);
     });
+});
+
+app.get("/estoque", async function(req, res){
+    const estoque = conexao.db("sysexp").collection("estoque");
+    const resultado = await estoque.find({}).toArray();
+    res.json(resultado);
+});
+
+// route dinamica
+app.get("/estoque/:id", function(req, res){
+    res.json(req.params)
 });
 
 app.listen(port, () => 
